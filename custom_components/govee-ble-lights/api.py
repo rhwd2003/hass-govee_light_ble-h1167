f096aa5afab422fdcb83982bd919c0fe4958395a
timniklas <accounts@tnkoch.de>
2024-12-19 19:40:45 +0100

    added color value check

--- custom_components/govee-ble-lights/api.py
+++ custom_components/govee-ble-lights/api.py
@@ -63,6 +63,12 @@ class GoveeAPI:
         await self._send(LedCommand.BRIGHTNESS, [value])
         
     async def set_color(self, red: int, green: int, blue: int):
+        if not 0 <= red <= 255:
+            raise ValueError(f'Color out of range: {red}')
+        if not 0 <= green <= 255:
+            raise ValueError(f'Color out of range: {green}')
+        if not 0 <= blue <= 255:
+            raise ValueError(f'Color out of range: {blue}')
         await self._send(LedCommand.COLOR, [0x15, 0x01, red, green, blue, 0, 0, 0, 0, 0, 0xff, 0xff])
         #legacy devices
         await self._send(LedCommand.COLOR, [0x02, red, green, blue])
