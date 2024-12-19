df12478f5fde1b42efdb55a8e6fae8644927a0a7
timniklas <accounts@tnkoch.de>
2024-12-19 19:53:32 +0100

    only send data when value is changed

--- custom_components/govee-ble-lights/light.py
+++ custom_components/govee-ble-lights/light.py
@@ -9,7 +9,6 @@ from homeassistant.components import bluetooth
 from homeassistant.components.light import (ColorMode, LightEntity, ATTR_BRIGHTNESS, ATTR_RGB_COLOR)
 
 from .api import GoveeAPI
-
 from .const import DOMAIN
 
 async def async_setup_entry(
@@ -65,28 +64,42 @@ class GoveeBluetoothLight(LightEntity):
         """Return the current rgw color."""
         return self._rgb
 
+    async def _setState(self, state: bool):
+        if self._state is state:
+            return None
+        await self._api.set_state(state)
+        self._state = state
+
+    async def _setBrightness(self, brightness: int):
+        if self._brightness is brightness:
+            return None
+        await self._api.set_brightness(brightness)
+        self._brightness = brightness
+
+    async def _setColor(self, red: int, green: int, blue: int):
+        if self._rgb is (red, green, blue):
+            return None
+        await self._api.set_color(red, green, blue)
+        self._rgb = (red, green, blue)
+
     async def async_turn_on(self, **kwargs):
         """Turn device on."""
         await self._api.connect(self._address)
-        await self._api.set_state(True)
+        await self._setState(True)
 
         if ATTR_BRIGHTNESS in kwargs:
             brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
-            await self._api.set_brightness(brightness)
-            self._brightness = brightness
+            await self._setBrightness(brightness)
 
         if ATTR_RGB_COLOR in kwargs:
             red, green, blue = kwargs.get(ATTR_RGB_COLOR)
-            await self._api.set_color(red, green, blue)
-            self._rgb = (red, green, blue)
+            await self._setColor(red, green, blue)
 
         await self._api.disconnect()
-        self._state = True
 
     
     async def async_turn_off(self, **kwargs):
         """Turn device off."""
         await self._api.connect(self._address)
-        await self._api.set_state(False)
+        await self._setState(False)
         await self._api.disconnect()
-        self._state = False
