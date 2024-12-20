30931c90da635ba2f54a2583b465537c525ce9b4
timniklas <accounts@tnkoch.de>
2024-12-20 12:31:04 +0100

    updated config flow to support segmented leds

--- custom_components/govee-ble-lights/__init__.py
+++ custom_components/govee-ble-lights/__init__.py
@@ -1,39 +1,59 @@
 from __future__ import annotations
 
+from enum import IntEnum
 from dataclasses import dataclass
-
 from homeassistant.components import bluetooth
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.const import CONF_ADDRESS, CONF_NAME
 
 from .const import DOMAIN
+import logging
+_LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[Platform] = [Platform.LIGHT]
+
+class BlePacket(IntEnum):
+    STATUS = 34819
 
 @dataclass
 class RuntimeData:
     """Class to hold your data."""
 
-    address : str
+    device_address: str
+    device_name: str
+    device_segmented: str
+    device_state: bool
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up Integration from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
 
-    address = config_entry.unique_id
-    assert address is not None
-    ble_device = bluetooth.async_ble_device_from_address(hass, address.upper(), True)
-    if not ble_device:
+    device_address = config_entry.data[CONF_ADDRESS]
+    device_name = config_entry.data[CONF_NAME]
+    device_segmented = config_entry.data["segmented"]
+
+    #look for device
+    if not bluetooth.async_ble_device_from_address(hass, device_address, True):
         raise ConfigEntryNotReady(
             f"Could not find LED BLE device with address {address}"
         )
+    
+    #get device status
+    service_info = bluetooth.async_last_service_info(hass, device_address, connectable=False)
+    device_state = None
+    if BlePacket.STATUS in service_info.manufacturer_data:
+        device_state = service_info.manufacturer_data[BlePacket.STATUS][4] == 1
 
     # Add the coordinator and update listener to hass data to make
     hass.data[DOMAIN][config_entry.entry_id] = RuntimeData(
-        address.upper()
+        device_address = device_address,
+        device_name = device_name,
+        device_segmented = device_segmented,
+        device_state = device_state
     )
 
     # Setup platforms (based on the list of entity types in PLATFORMS defined above)
@@ -60,3 +80,18 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> 
 
     # Return that unloading was successful.
     return unload_ok
+
+async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Migrate old entry."""
+    _LOGGER.debug("Migrating configuration from version %s", config_entry.version)
+
+    if config_entry.version == 1:
+        hass.config_entries.async_update_entry(config_entry, data={
+            CONF_ADDRESS: config_entry.unique_id.upper(),
+            CONF_NAME: config_entry.title,
+            "segmented": True
+        }, version=2)
+
+    _LOGGER.debug("Migration to configuration version %s successful", config_entry.version)
+
+    return True
