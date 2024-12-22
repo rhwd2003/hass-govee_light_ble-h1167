f0880aae98a7b8f4355f2b527677bba55f8ecc54
timniklas <accounts@tnkoch.de>
2024-12-22 01:38:22 +0100

    moved to buffered transmissions

--- custom_components/govee-ble-lights/api.py
+++ custom_components/govee-ble-lights/api.py
@@ -3,74 +3,116 @@ from colour import Color
 import asyncio
 import bleak_retry_connector
 from bleak import BleakClient
+from dataclasses import dataclass
 
-UUID_CONTROL_CHARACTERISTIC = '00010203-0405-0607-0809-0a0b0c0d2b11'
+from .const import WRITE_CHARACTERISTIC_UUID, READ_CHARACTERISTIC_UUID
 
-class LedCommand(IntEnum):
+class LedPacketHead(IntEnum):
+    COMMAND = 0x33
+    REQUEST = 0xaa
+
+class LedPacketCmd(IntEnum):
     POWER      = 0x01
     BRIGHTNESS = 0x04
     COLOR      = 0x05
 
+class LedColorType(IntEnum):
+    SEGMENTS    = 0x15
+    SINGLE      = 0x02
+
+@dataclass
+class LedPacket:
+    head: LedPacketHead
+    cmd: LedPacketCmd
+    payload: bytes | list = b''
+
+@dataclass
+class LedFrame:
+    uuid: str
+    data: bytes
+
+def generateChecksum(frame: bytes):
+    # The checksum is calculated by XORing all data bytes
+    checksum = 0
+    for b in frame:
+        checksum ^= b
+    return bytes([checksum & 0xFF])
+
+def getUUIDByHead(head: LedPacketHead):
+    match head:
+        case LedPacketHead.REQUEST:
+            return READ_CHARACTERISTIC_UUID
+        case _:
+            return WRITE_CHARACTERISTIC_UUID
+
 class GoveeAPI:
-    def __init__(self, ble_device):
+    def __init__(self, ble_device, address):
         self._conn = None
         self._ble_device = ble_device
-
-    @property
-    def connected(self):
-        return not self._conn is None
+        self._address = address
+        self._frame_buffer = []
     
-    async def connect(self, address: str):
-        self._conn = await bleak_retry_connector.establish_connection(BleakClient, self._ble_device, address)
+    async def _prepareWritePacket(self, packet: LedPacket):
+        await self._preparePacket(WRITE_CHARACTERISTIC_UUID, packet)
+    
+    async def _prepareReadPacket(self, packet: LedPacket):
+        await self._preparePacket(READ_CHARACTERISTIC_UUID, packet)
 
-    async def disconnect(self):
-        if self.connected:
-            await self._conn.disconnect()
-
-    async def _send(self, cmd, payload):
-        if not self.connected:
-            raise Exception('not connected to bluetooth device')
-        if not isinstance(cmd, int):
-            raise ValueError('Invalid command')
-        if not isinstance(payload, bytes) and not (isinstance(payload, list) and all(isinstance(x, int) for x in payload)):
-            raise ValueError('Invalid payload')
-        if len(payload) > 17:
-            raise ValueError('Payload too long')
-
-        cmd = cmd & 0xFF
-        payload = bytes(payload)
-
-        frame = bytes([0x33, cmd]) + bytes(payload)
+    async def _preparePacket(self, packet: LedPacket):
+        uuid = getUUIDByHead(packet.head)
+        cmd = packet.cmd & 0xFF
+        payload = bytes(packet.payload)
+        frame = bytes([packet.head, cmd]) + bytes(payload)
         # pad frame data to 19 bytes (plus checksum)
         frame += bytes([0] * (19 - len(frame)))
-        
-        # The checksum is calculated by XORing all data bytes
-        checksum = 0
-        for b in frame:
-            checksum ^= b
-        
-        frame += bytes([checksum & 0xFF])
-        await self._conn.write_gatt_char(UUID_CONTROL_CHARACTERISTIC, frame, False)
-        await asyncio.sleep(.01)
+        frame += generateChecksum(frame)
+        self._frame_buffer.append(
+            LedFrame(uuid, frame)
+        )
     
-    async def set_state(self, state: bool):
-        await self._send(LedCommand.POWER, [0x1 if state else 0x0])
+    async def _getClient(self):
+        return await bleak_retry_connector.establish_connection(BleakClient, self._ble_device, self._address)
+
+    async def sendPacketBuffer(self):
+        if len(self._frame_buffer) == 0:
+            return None #nothing to do
+        async with await self._getClient() as client:
+            for frame in self._frame_buffer:
+                await client.write_gatt_char(frame.uuid, frame.data, False)
+            self._frame_buffer = []
     
-    async def set_brightness(self, value: int):
+    async def setStateBuffered(self, state: bool):
+        packet = LedPacket(
+            head=LedPacketHead.COMMAND,
+            cmd=LedPacketCmd.POWER,
+            payload=[0x1 if state else 0x0]
+        )
+        await self._preparePacket(packet)
+    
+    async def setBrightnessBuffered(self, value: int):
         if not 0 <= value <= 255:
             raise ValueError(f'Brightness out of range: {value}')
         value = round(value)
-        await self._send(LedCommand.BRIGHTNESS, [value])
+        packet = LedPacket(
+            head=LedPacketHead.COMMAND,
+            cmd=LedPacketCmd.BRIGHTNESS,
+            payload=[value]
+        )
+        await self._preparePacket(packet)
         
-    async def set_color(self, red: int, green: int, blue: int, segmented: bool = False):
+    async def setColorBuffered(self, red: int, green: int, blue: int, segmented: bool = False):
         if not 0 <= red <= 255:
             raise ValueError(f'Color out of range: {red}')
         if not 0 <= green <= 255:
             raise ValueError(f'Color out of range: {green}')
         if not 0 <= blue <= 255:
             raise ValueError(f'Color out of range: {blue}')
-        
+        payload = [LedColorType.SINGLE, red, green, blue]
         if segmented:
-            await self._send(LedCommand.COLOR, [0x15, 0x01, red, green, blue, 0, 0, 0, 0, 0, 0xff, 0xff])
-        else:
-            await self._send(LedCommand.COLOR, [0x02, red, green, blue])
+            payload = [LedColorType.SEGMENTS, 0x01, red, green, blue, 0, 0, 0, 0, 0, 0xff, 0xff]
+        packet = LedPacket(
+            head=LedPacketHead.COMMAND,
+            cmd=LedPacketCmd.COLOR,
+            payload=payload
+        )
+        await self._preparePacket(packet)
