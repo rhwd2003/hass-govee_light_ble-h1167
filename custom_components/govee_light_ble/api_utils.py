6a237c2e2c5069d76856ae9d442de595c8be7d3b
timniklas <accounts@tnkoch.de>
2024-12-26 15:27:31 +0100

    moved functions to api_utils.py

---
+++ custom_components/govee_light_ble/api_utils.py
@@ -0,0 +1,36 @@
+from enum import IntEnu
+from dataclasses import dataclass
+
+class LedPacketHead(IntEnum):
+    COMMAND = 0x33
+    REQUEST = 0xaa
+
+class LedPacketCmd(IntEnum):
+    POWER      = 0x01
+    BRIGHTNESS = 0x04
+    COLOR      = 0x05
+    SEGMENT    = 0xa5
+
+class LedColorType(IntEnum):
+    SEGMENTS    = 0x15
+    SINGLE      = 0x02
+
+@dataclass
+class LedPacket:
+    head: LedPacketHead
+    cmd: LedPacketCmd
+    payload: bytes | list = b''
+
+def generateChecksum(frame: bytes):
+    # The checksum is calculated by XORing all data bytes
+    checksum = 0
+    for b in frame:
+        checksum ^= b
+    return bytes([checksum & 0xFF])
+
+def generateFrame(packet: LedPacket):
+    cmd = packet.cmd & 0xFF
+    frame = bytes([packet.head, cmd]) + bytes(packet.payload)
+    # pad frame data to 19 bytes (plus checksum)
+    frame += bytes([0] * (19 - len(frame)))
+    frame += generateChecksum(frame)
