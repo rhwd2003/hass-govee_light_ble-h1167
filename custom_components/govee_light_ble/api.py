2e65923bf6f1b29b043a70c9f131c984332fb53d
timniklas <accounts@tnkoch.de>
2024-12-27 22:12:13 +0100

    optimized connection performance

--- custom_components/govee_light_ble/api.py
+++ custom_components/govee_light_ble/api.py
@@ -18,22 +18,30 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 
 class GoveeAPI:
-    def __init__(self, ble_device: BLEDevice, address: str, segmented: bool = False):
+    state: bool | None = None
+    brightness: int | None = None
+    color: tuple[int, ...] | None = None
+
+    def __init__(self, ble_device: BLEDevice, segmented: bool = False):
         self._conn = None
         self._ble_device = ble_device
-        self._address = address
         self._segmented = segmented
         self._packet_buffer = []
         self._expected_responses = []
         self._client = None
-        self.state = None
-        self.brightness  = None
-        self.color = None
+
+    @property
+    def address(self):
+        return self._ble_device.address
+
+    @property
+    def _responseExpected(self):
+        return len(self._expected_responses) > 0
 
     async def _ensureConnected(self):
         """ connects to a bluetooth device """
         if self._client ==  None:
-            self._client = await bleak_retry_connector.establish_connection(BleakClient, self._ble_device, self._address)
+            self._client = await bleak_retry_connector.establish_connection(BleakClient, self._ble_device, self.address)
 
     async def _disconnect(self):
         """ disconnects from a bluetooth device """
@@ -42,37 +50,44 @@ class GoveeAPI:
             self._client = None
 
     async def _transmitPacket(self, packet: LedPacket):
+        """ transmit the actiual packet """
         #convert to bytes
         frame = await GoveeUtils.generateFrame(packet)
         #transmit to UUID
         await self._client.write_gatt_char(WRITE_CHARACTERISTIC_UUID, frame, False)
 
-    async def _notificationHandler(self, characteristic: BleakGATTCharacteristic, frame: bytearray):
+    async def _handleRequest(self, packet: LedPacket):
+        """ process received responses """
+        match packet.cmd:
+            case LedPacketCmd.POWER:
+                self.state = packet.payload[0] == 0x01
+            case LedPacketCmd.BRIGHTNESS:
+                #segmented devices 0-100
+                self.brightness = packet.payload[0] / 100 * 255 if self._segmented else packet.payload[0]
+            case LedPacketCmd.COLOR:
+                red = packet.payload[1]
+                green = packet.payload[2]
+                blue = packet.payload[3]
+                self.color = (red, green, blue)
+            case LedPacketCmd.SEGMENT:
+                red = packet.payload[2]
+                green = packet.payload[3]
+                blue = packet.payload[4]
+                self.color = (red, green, blue)
+
+    async def _handleReceive(self, characteristic: BleakGATTCharacteristic, frame: bytearray):
+        """ receives packets async """
         if not await GoveeUtils.verifyChecksum(frame):
             raise Exception("transmission error, received packet with bad checksum")
-
+        
         packet = LedPacket(
             head=frame[0],
             cmd=frame[1],
             payload=frame[2:-1]
         )
+        #only requests are expected to send a response
         if packet.head == LedPacketHead.REQUEST:
-            match packet.cmd:
-                case LedPacketCmd.POWER:
-                    self.state = packet.payload[0] == 0x01
-                case LedPacketCmd.BRIGHTNESS:
-                    #segmented devices 0-100
-                    self.brightness = packet.payload[0] / 100 * 255 if self._segmented else packet.payload[0]
-                case LedPacketCmd.COLOR:
-                    red = packet.payload[1]
-                    green = packet.payload[2]
-                    blue = packet.payload[3]
-                    self.color = (red, green, blue)
-                case LedPacketCmd.SEGMENT:
-                    red = packet.payload[2]
-                    green = packet.payload[3]
-                    blue = packet.payload[4]
-                    self.color = (red, green, blue)
+            await self._handleRequest(packet)
             #remove from excpeted commands list if in it
             if packet.cmd in self._expected_responses:
                 self._expected_responses.remove(packet.cmd)
@@ -90,25 +105,36 @@ class GoveeAPI:
         for index in range(1 if request else repeat):
             self._packet_buffer.append(packet)
 
+    async def _clearPacketBuffer(self):
+        """ clears the packet buffer """
+        self._packet_buffer = []
+    
+    async def _startReceiving(self):
+        """ start receiving packets """
+        self.stop_event = asyncio.Event()
+        await self._client.start_notify(READ_CHARACTERISTIC_UUID, self._handleReceive)
+
+    async def _stopReceiving(self):
+        """ stop receiving packets """
+        await self._client.stop_notify(READ_CHARACTERISTIC_UUID)
+
     async def sendPacketBuffer(self):
         """ transmits all buffered data """
-        if len(self._packet_buffer) == 0:
-            #nothing to do
+        if not self._packet_buffer:
+            #buffer is empty
             return None
         await self._ensureConnected()
-        waitForResponses = len(self._expected_responses) > 0
-        if waitForResponses:
-            self.stop_event = asyncio.Event()
-            await self._client.start_notify(READ_CHARACTERISTIC_UUID, self._notificationHandler)
+        if self._responseExpected:
+            await self._startReceiving()
         
         for packet in self._packet_buffer:
             await self._transmitPacket(packet)
-        #clear buffer
-        self._packet_buffer = []
+        await self._clearPacketBuffer()
 
-        if waitForResponses:
+        if self._responseExpected:
+            #wait to receive all exptected packets
             await self.stop_event.wait()
-            await self._client.stop_notify(READ_CHARACTERISTIC_UUID)
+            await self._stopReceiving()
         await self._disconnect()
 
     async def requestStateBuffered(self):
