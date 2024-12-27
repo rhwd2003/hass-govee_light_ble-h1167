124f3d40d39b7395fb3117efe5251b88841020e1
timniklas <accounts@tnkoch.de>
2024-12-27 04:16:54 +0100

    Added buffered transmission auf requests

--- custom_components/govee_light_ble/api.py
+++ custom_components/govee_light_ble/api.py
@@ -24,6 +24,7 @@ class GoveeAPI:
         self._address = address
         self._segmented = segmented
         self._packet_buffer = []
+        self._expected_responses = []
         self._client = None
         self.state = None
         self.brightness  = None
@@ -34,47 +35,55 @@ class GoveeAPI:
         if self._client ==  None:
             self._client = await bleak_retry_connector.establish_connection(BleakClient, self._ble_device, self._address)
 
-    async def disconnect(self):
+    async def _disconnect(self):
         """ disconnects from a bluetooth device """
         if self._client !=  None:
             await self._client.disconnect()
             self._client = None
 
     async def _transmitPacket(self, packet: LedPacket):
-        await self._ensureConnected()
         #convert to bytes
         frame = GoveeUtils.generateFrame(packet)
         #transmit to UUID
         await self._client.write_gatt_char(WRITE_CHARACTERISTIC_UUID, frame, False)
 
-    def _notification_handler(self, characteristic: BleakGATTCharacteristic, frame: bytearray):
+    def _notificationHandler(self, characteristic: BleakGATTCharacteristic, frame: bytearray):
         if not GoveeUtils.verifyChecksum(frame):
-            _LOGGER.error("discard packet with bad checksum")
-            self.stop_event.set() #checksum bad
-            return None
+            raise Exception("transmission error, received packet with bad checksum")
 
-        self.response_packet = LedPacket(
-            head=hex(frame[0]),
-            cmd=hex(frame[1]),
+        packet = LedPacket(
+            head=frame[0],
+            cmd=frame[1],
             payload=frame[2:-1]
         )
-        self.stop_event.set()
-
-    async def _sendPacketWithResponse(self, cmd: LedPacketCmd, payload: bytes | list = b''):
-        await self._ensureConnected()
-        self.response_packet = None
-        self.stop_event = asyncio.Event()
-        await self._client.start_notify(READ_CHARACTERISTIC_UUID, self._notification_handler)
-        #transmit request
-        packet = LedPacket(LedPacketHead.REQUEST, cmd, payload)
-        await self._transmitPacket(packet)
-        #wait for response
-        await self.stop_event.wait()
-        await self._client.stop_notify(READ_CHARACTERISTIC_UUID)
-        return self.response_packet
+        if packet.head == LedPacketHead.REQUEST:
+            match packet.cmd:
+                case LedPacketCmd.POWER:
+                    self.state = packet.payload[0] == 0x01
+                case LedPacketCmd.BRIGHTNESS:
+                    #segmented devices 0-100
+                    self.brightness = packet.payload[0] / 100 * 255 if self._segmented else packet.payload[0]
+                case LedPacketCmd.COLOR:
+                    red = packet.payload[1]
+                    green = packet.payload[2]
+                    blue = packet.payload[3]
+                    self.color = (red, green, blue)
+                case LedPacketCmd.SEGMENT:
+                    red = packet.payload[2]
+                    green = packet.payload[3]
+                    blue = packet.payload[4]
+                    self.color = (red, green, blue)
+            #remove from excpeted commands list if in it
+            if packet.cmd in self._expected_responses:
+                self._expected_responses.remove(packet.cmd)
+        #check if all packets are received
+        if len(self._expected_responses) == 0:
+            self.stop_event.set()
 
     async def _preparePacket(self, cmd: LedPacketCmd, payload: bytes | list = b'', request: bool = False, repeat: int = 3):
         """ add data to transmission buffer """
+        if request and cmd not in self._expected_responses:
+            self._expected_responses.append(cmd)
         #request data or perform a change
         head = LedPacketHead.REQUEST if request else LedPacketHead.COMMAND
         packet = LedPacket(head, cmd, payload)
@@ -86,44 +95,38 @@ class GoveeAPI:
         if len(self._packet_buffer) == 0:
             #nothing to do
             return None
+        await self._ensureConnected()
+        waitForResponses = len(self._expected_responses) > 0
+        if waitForResponses:
+            self.stop_event = asyncio.Event()
+            await self._client.start_notify(READ_CHARACTERISTIC_UUID, self._notificationHandler)
+        
         for packet in self._packet_buffer:
             await self._transmitPacket(packet)
         #clear buffer
         self._packet_buffer = []
 
-    async def requestState(self):
+        if waitForResponses:
+            await self.stop_event.wait()
+            await self._client.stop_notify(READ_CHARACTERISTIC_UUID)
+        await self._disconnect()
+
+    async def requestStateBuffered(self):
         """ adds a request for the current power state to the transmit buffer """
-        packet = await self._sendPacketWithResponse(LedPacketCmd.POWER)
-        assert packet
-        self.state = packet.payload[0] == 0x01
+        await self._preparePacket(LedPacketCmd.POWER, request=True)
 
-    async def requestBrightness(self):
+    async def requestBrightnessBuffered(self):
         """ adds a request for the current brightness state to the transmit buffer """
-        packet = await self._sendPacketWithResponse(LedPacketCmd.BRIGHTNESS)
-        assert packet
-        if self._segmented:
-            #segmented devices 0-100
-            return packet.payload[0] / 100 * 255
-        self.brightness = packet.payload[0]
+        await self._preparePacket(LedPacketCmd.BRIGHTNESS, request=True)
 
-    async def requestColor(self):
+    async def requestColorBuffered(self):
         """ adds a request for the current color state to the transmit buffer """
         if self._segmented:
             #0x01 means first segment
-            packet = await self._sendPacketWithResponse(LedPacketCmd.SEGMENT, b'\x01')
-            assert packet
-            red = packet.payload[2]
-            green = packet.payload[3]
-            blue = packet.payload[4]
-            self.color = (red, green, blue)
+            await self._preparePacket(LedPacketCmd.SEGMENT, b'\x01', request=True)
         else:
             #empty response on segmented devices
-            packet = await self._sendPacketWithResponse(LedPacketCmd.COLOR)
-            assert packet
-            red = packet.payload[1]
-            green = packet.payload[2]
-            blue = packet.payload[3]
-            self.color = (red, green, blue)
+            packet = await self._preparePacket(LedPacketCmd.COLOR, request=True)
     
     async def setStateBuffered(self, state: bool):
         """ adds the state to the transmit buffer """
